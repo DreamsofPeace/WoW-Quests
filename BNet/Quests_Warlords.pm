5e7a7dd1d9cbbab502c0c8d7eec5086b75bd37e1
DreamsofPeace <31104853+DreamsofPeace@users.noreply.github.com>
2017-09-26 09:45:55 -0400

    Warlords Intro Quests

--- BNet/Quests_Warlords.pm
+++ BNet/Quests_Warlords.pm
@@ -4,11 +4,77 @@ use strict;
 use warnings;
 
 
-sub z_68_72_borean_tundra ($%) {
+sub z_90_blasted_lands_intro ($%) {
+	my ($count, %multiuserquests) = @_;
+	BNet::Utils::groupheader ("90: Blasted Lands: Warlords Intro (Unsorted)");
+
+	BNet::Utils::preprint ($count, '35460', "Attack of the Iron Horde <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35462', "Under Siege <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35463', "Subversive Scouts <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35464', "Ending Executions <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35486', "Lunatic Lieutenants <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35488', "Death to the Dreadmaul <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35492', "For Nethergarde! <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35494', "Mementos of the Fallen <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35495', "Toothsmash the Annihilator <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35496', "Enohar's Revenge <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35499', "Blow up towers", %multiuserquests);
+	BNet::Utils::preprint ($count, '35500', "Gar'mak Bladetwist <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35744', "Ending Executions <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35745', "Attack of the Iron Horde <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35746', "Under Siege <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35748', "Subversive Scouts <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35750', "Lunatic Lieutenants <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35751', "Attack on Nethergarde <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35760', "Toothsmash the Annihilator <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35761', "Ransacking Nethergarde <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35762', "Death to the Dreadmaul <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35763', "Gar'mak Bladetwist <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '35764', "Okrilla's Revenge <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36292', "Investigating the Invasion <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36307', "Investigating the Invasion <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36379', "Peeking into the Portal <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36382', "Peeking into the Portal <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36498', "Iron Horde Invasion <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36499', "Iron Horde Invasion <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36940', "Warning Orgrimmar <img src=\"http://wow.zamimg.com/images/icons/horde.png\">", %multiuserquests);
+	BNet::Utils::preprint ($count, '36941', "Report to the King <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+
+
+}
+
+sub z_90_92_frostfire_ridge ($%) {
 	my ($count, %multiuserquests) = @_;
 
-	BNet::Utils::groupheader ("68: Borean Tundra: Intro");
-	BNet::Utils::preprint ($count, '28709', "Hero's Call: Northrend!  <img src=\"http://wow.zamimg.com/images/icons/alliance.png\">", %multiuserquests);
+}
+
+sub z_90_92_shadowmoon_valley ($%) {
+	my ($count, %multiuserquests) = @_;
+
+}
+
+sub z_92_94_gorgrond ($%) {
+	my ($count, %multiuserquests) = @_;
+
+}
+
+sub z_94_96_talador ($%) {
+	my ($count, %multiuserquests) = @_;
+
+}
+
+sub z_96_98_spires_of_arak ($%) {
+	my ($count, %multiuserquests) = @_;
+
+}
+
+sub z_98_100_nagrand ($%) {
+	my ($count, %multiuserquests) = @_;
+
+}
+
+sub z_100_tanaan_jungle ($%) {
+	my ($count, %multiuserquests) = @_;
 
 }
 
