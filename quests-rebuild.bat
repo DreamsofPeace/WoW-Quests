d4f07f5992cfb40929d449be2a55b9957525813c
DreamsofPeace <31104853+DreamsofPeace@users.noreply.github.com>
2018-10-24 16:05:53 -0400

    Auto-build-resources

---
+++ quests-rebuild.bat
@@ -0,0 +1,7 @@
+quest-gen.py Resources\quest-list-bc.txt Quests_BC > BNet\Quests_BC.pm
+quest-gen.py Resources\quest-list-wrath.txt Quests_Wrath > BNet\Quests_Wrath.pm
+quest-gen.py Resources\quest-list-ek.txt Quests_EK > BNet\Quests_EK.pm
+quest-gen.py Resources\quest-list-kd.txt Quests_KD > BNet\Quests_KD.pm
+quest-gen.py Resources\quest-list-bfa.txt Quests_BFA > BNet\Quests_BFA.pm
+quest-gen.py Resources\quest-list-cata.txt Quests_Cata > BNet\Quests_Cata.pm
+quest-gen.py Resources\quest-list-legion.txt Quests_Legion > BNet\Quests_Legion.pm
