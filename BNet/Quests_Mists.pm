b8fe4b9e94d39ae720abba4c6a97fab38b8855a0
DreamsofPeace <31104853+DreamsofPeace@users.noreply.github.com>
2018-11-07 14:41:42 -0500

    Programmatically rebuilding mists quests.

--- BNet/Quests_Mists.pm
+++ BNet/Quests_Mists.pm
@@ -3,70 +3,83 @@ package BNet::Quests_Mists;
 use strict;
 use warnings;
 
-sub sz_01_10_Wandering_Isle ($%) {
+sub sz_01_10_Wandering_Isle($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("01-10: Wandering Isle");
 
 }
 
-sub z_80_90_Jade_Forest ($%) {
+sub z_80_90_Jade_Forest($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("80-90: Jade Forest");
 
 }
 
-sub z_81_90_Valley_of_the_Four_Winds ($%) {
+sub z_81_90_Valley_of_the_Four_Winds($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("81-90: Valley of the Four Winds");
 
 }
 
-sub z_81_90_Krasarang_Wilds ($%) {
+sub z_81_90_Krasarang_Wilds($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("81-90: Krasarang Wilds");
 
 }
 
-sub z_82_90_KunLai_Summit ($%) {
+sub z_82_90_KunLai_Summit($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("82-90: KunLai Summit");
 
 }
 
-sub z_83_90_Townlong_Steppes ($%) {
+sub z_83_90_Townlong_Steppes($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("83-90: Townlong Steppes");
 
 }
 
-sub z_84_90_Dread_Wastes ($%) {
+sub z_84_90_Dread_Wastes($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("84-90: Dread Wastes");
 
 }
 
-sub z_85_90_Vale_of_Eternal_Blossoms ($%) {
+sub z_85_90_Vale_of_Eternal_Blossoms($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("85-90: Vale of Eternal Blossoms");
 
 }
 
-sub z_85_90_Isle_of_Thunder ($%) {
+sub z_85_90_Isle_of_Giants($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("85-90: Isle of Giants");
 
 }
 
-sub z_85_90_Isle_of_Giants ($%) {
+sub z_85_90_Isle_of_Thunder($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("85-90: Isle of Thunder");
 
 }
 
-sub z_85_90_Timeless_Isle ($%) {
+sub z_85_90_Timeless_Isle($%) {
 	my ($count, %multiuserquests) = @_;
 
+	BNet::Utils::groupheader ("85-90: Timeless Isle");
 
 }
+
 
 1;
+
